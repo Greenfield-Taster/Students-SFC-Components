eb25d31289956d9d2a13f9512ffc5407888c973c
Asya Gorbatchova <gorbatchovaasa123@gmail.com>
2023-10-24 19:49:42 +0300

    Add vue-router, props, server(add, delete student)

---
+++ src/components/StudentInfo.vue
@@ -0,0 +1,24 @@
+<template>
+    {{ student.name }}
+</template>
+
+<script >
+import axios from 'axios';
+
+export default ({
+    props:{
+        id:'',
+    },
+    data(){
+        return{
+            student:{},
+
+        }
+    },
+    mounted () {
+       axios.get("http://34.82.81.113:3000/students/"+this.id)
+        .then(data => {
+            this.student =  data.data;
+        })},
+})
+</script>
---
+++ src/components/Students.vue
@@ -0,0 +1,86 @@
+<template>
+<div id="app">
+  <h1>Searching</h1>
+      <input placeholder="type name" v-model="searchName" />
+      <table>
+        <tr v-for="(stud, index) in students" :key="index">
+          <td :class="{ 'red': stud.highlighted }">
+            <router-link v-bind:to="'/student-info/'+stud._id">
+            {{stud.name}}
+          </router-link>
+        </td>
+          <td>
+            <input type="checkbox" v-model="stud.workDone" />
+          </td>
+          <td>{{ stud.group }}</td>
+          <td>{{ stud.grade }}</td>
+          <td><a @click="deleteStudent(stud._id)">Delete</a></td>
+        </tr>
+      </table>
+
+      <div class="addStudent">
+        <input placeholder="add name" v-model="newStudent.name" />
+        <input v-model="newStudent.workDone" type="checkbox" />
+        <select v-model="newStudent.group" id="myComboBox" name="group">
+          <option value="placeholder" disabled selected>Group:</option>
+          <option value="RPZ 20 1/9">RPZ 20 1/9</option>
+          <option value="RPZ 20 2/9">RPZ 20 2/9</option>
+        </select>
+        <input v-model="newStudent.grade" type="number" placeholder="grade" />
+        <button @click="addStudent()">Add student</button>
+      </div>
+</div>  
+</template>
+
+<script >
+import axios from 'axios'
+
+
+export default {
+    data() {
+       return {
+           students: [],
+           searchName:'',
+           newStudent: {id:"" ,name: "", workDone: false, group: ""}
+       }
+    },
+    mounted () {
+       axios.get("http://34.82.81.113:3000/students")
+        .then(data => {
+            this.students =  data.data;
+        })},
+    methods: {
+      deleteStudent(id) {
+     axios.delete(`http://34.82.81.113:3000/students/${id}`).then(data => {
+          console.log(data);   
+             const index = this.students.findIndex((stud) => stud._id === id);
+       if (index !== -1) {
+         this.students.splice(index, 1);
+       }
+ })
+    },
+      addStudent() {
+      // this.newStudent.id = this.students.length + 1;
+      axios.post("http://34.82.81.113:3000/students", this.newStudent).then(response => {console.log(response.data);
+      this.students.push(response.data);})
+
+      this.newStudent.name = "";
+      this.newStudent.workDone = false;
+      this.newStudent.group = "";
+      this.newStudent.grade = "";
+    },
+    computed: {
+      filteredStudents: function () {
+        const searchNameLower = this.searchName.toLowerCase();
+        return this.students.map((stud) => ({
+          ...stud,
+          highlighted:
+            stud.name.toLowerCase().includes(searchNameLower) &&
+            searchNameLower.length > 0,
+        }));
+      },
+    },
+  },
+
+}
+</script>
